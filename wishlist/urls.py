9c9b410deeee8f2e06fbf5e674324cdafdb10ca3
syafiqoarashy <101589777+syafiqoarashy@users.noreply.github.com>
2022-09-15 11:01:30 +0700

    completed tutorial 2

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from wishlist.views import show_wishlist
+from wishlist.views import show_data_xml
+from wishlist.views import show_data_json
+from wishlist.views import show_data_xml_id
+from wishlist.views import show_data_json_id
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', show_data_xml, name='show_xml'),
+    path('json/', show_data_json, name='show_json'),
+    path('json/<int:id>', show_data_json_id, name='show_data_jason_id'),
+    path('xml/<int:id>', show_data_xml_id, name='show_data_xml_id'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from wishlist.models import ItemWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_wishlist(request):
@@ -9,4 +11,19 @@ def show_wishlist(request):
         'name': 'Syafiqo Arashy Octaviano'
     }   
     return render(request, "wishlist.html", context)
-    
+
+def show_data_xml(request):
+    data = ItemWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_data_json(request):
+    data = ItemWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_data_xml_id(request,id):
+    data = ItemWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_data_json_id(request,id):
+    data = ItemWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
